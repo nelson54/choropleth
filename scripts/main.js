f1385d8c17efe3afbe2b4324d5429ba31afabc83
Derek Nelson <nelson54@ferris.edu>
2013-08-13 21:05:58 -0400

    Remove 3d transform and legend Add tooltips

--- scripts/main.js
+++ scripts/main.js
@@ -8,328 +8,159 @@
 
 $(function(){
 
-    var g;
+    d3.json("http://localhost:3001/reportsback/map/snapshots/all", function(error, json) {
+        if (error) return console.warn(error);
+        console.log(JSON.stringify(json));
+        var options = json.map(function(snapshot){
+            var year = snapshot.year,
+                month = snapshot.month,
+                monthYear = month + "-" +year;
 
-    var data = d3.selectAll('.state')[0].map(
-        function(st){
+            return jQuery("<option></option>")
+                .text(monthYear)
+                .attr("value", monthYear);
+        });
+
+        jQuery("div.container").prepend(
+            jQuery("<select></select>")
+                .append(options)
+                .css("float", "left")
+        );
+    });
+
+    jQuery("svg")
+        .css("clear", "left")
+        .css("float", "left");
+
+    var svgWidth = d3.select("svg").attr("width"),
+        svgHeight = d3.select("svg").attr("height");
+
+    var _is_state_selected;
+
+    var range = d3.select("#color-range"),
+
+    colorRange = d3.range(10, 60, 10).map(function(num){
+        return range.attr('data-color-' + num);
+    }),
+    percentageFormatter = d3.format("%"),
+    data = d3.selectAll('.state')[0]
+        .map(function(st){
             var state = d3.select(st);
             var teachersTotal = state.attr("data-total");
             var teachers = state.attr("data-val");
-            var code = state.attr("id");
-            return {code: code, count: teachers, total: teachersTotal, percentage: teachersTotal/teachers}
-        }
-    );
+            var n = state.node().getBBox(),
 
-    var enrichDocument = function(){
+            color = state.style("fill"),
+            d3rgb = d3.rgb(color),
+            d3lab = d3.lab(d3rgb),
+            dColor = d3lab.darker().toString(),
+            lColor = d3lab.brighter().toString(),
+            code = state.attr("id"),
 
-        data = window.data = d3.map(d3.selectAll('.state')[0],
-            function(st){
-                var teachersTotal = parseInt(st.attr("data-total"),10);
-                var teachers = parseInt(st.attr("data-val"),10);
-                var code = st.attr("id");
-                return {code: code, count: teachers, total: teachersTotal, percentage: teachers/teachersTotal}
+            topLeft = [n.x, n.y],
+            bottomRight = [n.x + n.width, n.y + n.height],
+
+            //Calculate the height and width of the box
+            height = Math.abs(n.height),
+            width = Math.abs(n.width),
+
+            centerX = n.x + width / 2,
+            centerY = n.y + height / 2,
+            translationDifferential = .30,
+            x = -topLeft[0],
+            y = -topLeft[1],
+            fontSize = "10px",
+            scale,
+            fontSize,
+            translationDifferential;
+
+            if (height < 10 || width < 10){
+                scale = .25 / Math.max( height / svgHeight, width / svgWidth);
+                fontSize = "6px";
+            } else if (height < 30 || width < 30) {
+                scale = .50 / Math.max( height / svgHeight, width / svgWidth);
+            } else {
+                scale = .50 / Math.max( height / svgHeight, width / svgWidth);
+                translationDifferential = .40;
             }
-        );
 
-        g = d3.select("svg > g");
-        //g.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+            var transform = "translate(" + topLeft[0]*translationDifferential +","+ topLeft[1]*translationDifferential + ")"
+                + "scale(" + scale + ")"
+                + "translate(" + x + "," + y + ")";
 
-        drawLegend();
-
-        var states = g.selectAll(".state");
-
-        states[0].sort(function(state1, state2){
-            return d3.ascending($(state1).attr("id"),$(state2).attr("id"))
+            return {
+                    name: states[code],
+                    code: code,
+                    count: teachers,
+                    total: teachersTotal,
+                    percentage: teachers/teachersTotal,
+                    color: color,
+                    dColor: dColor,
+                    lColor: lColor,
+                    topLeft: topLeft,
+                    bottomRight: bottomRight,
+                    height: height,
+                    width: width,
+                    centerX: centerX,
+                    centerY: centerY,
+                    textX: -23 + centerX,
+                    textY: 10 + centerY,
+                    fontSize: fontSize,
+                    selectionTransform: transform
+                }
         });
 
-        states.each(doState);
-    };
+    var svg = d3.select("svg > g"),
+        listView = d3.select('ul#stateData');
 
-    var doState = function(){
-        var listView = d3.select('ul#stateData');
-        //var state = el;//d3.select(el);
+    data.forEach(function(state){
+        var percentage = percentageFormatter(state.percentage);
 
-        var state = d3.select(this)
-            .on("click", click)
-            .on("mouseover", highlightState)
-            .on("mouseout",  unhighlightState);
+        svg.select("#" + state.code)
+            .on("click", function(){
+                if(_is_state_selected){
+                    _is_state_selected = false;
 
-        listView.append("li")
-            .attr("data-state-id", state.attr("id"))
-            .append("a")
-            .attr("href", "javascript:void(0)")
-            .text(states[state.attr("id")] + " ")
-            .append("span")
-            .append("strong")
-            .property("class", "perc")
-            .text(statePercentage(state));
+                    d3.select("svg > g").transition()
+                        .duration(750)
+                        .style("stroke-width", "")
+                        .style("stroke-color", "")
+                        .attr("transform", "");
 
-        listView.selectAll("li")
-            .on("click", click)
-            .on("mouseover", highlightState)
-            .on("mouseout", unhighlightState);
-    }
-
-    var colorRange = function(){
-        var range = d3.select("#color-range"),
-            list = [];
-
-        list.push(range.attr('data-color-10'));
-        list.push(range.attr('data-color-20'));
-        list.push(range.attr('data-color-30'));
-        list.push(range.attr('data-color-40'));
-        list.push(range.attr('data-color-50'));
-
-        return list;
-    }
-
-    var drawLegend = function(){
-        var color = d3.scale.threshold()
-            .domain([.1, .2, .3, .4, .5])
-            .range(colorRange());
-
-        // A position encoding for the key only.
-        var x = d3.scale.linear()
-            .domain([0,.5])
-            .range([0, 350]);
-
-        var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient("bottom")
-            .tickSize(30)
-            .tickValues(color.domain())
-            .tickFormat(function(tick){return numeral(tick).format("0%")});
-
-        var key = d3.select("svg").attr("height", "670").append("g")
-            .attr("class", "key")
-            .attr("transform", "translate(0,600)");
-
-        key.call(xAxis).append("text")
-            .attr("class", "caption")
-            .attr("y", - 6)
-            .text("Percentage of Market")
-            .style("fill", "#000");
-
-        key.selectAll(".tick").style("fill", "#000")
-        d3.select("body")
-            .on("mousemove", function(){
-                if (!window.domain)
-                    window.domain = d3.select("path.domain")[0][0];
-
-                var mxy = d3.mouse(d3.select("path.domain")[0][0]);
-
-                if(window.over === true && mxy && 350 > mxy[0] && mxy[0] > 0 && mxy[1] > 0 && 30 > mxy[1]){
-
-
-
-                    var mousePosition = d3.mouse(window.domain)[0];
-                    var perc = (mousePosition/350)*.5,
-                        start = perc * .7,
-                        end = perc * 1.3;
-
-
-
-                    d3.selectAll(".state").each(
-                        function(){
-                            var state = d3.select(this);
-                            var statePerc = stateNumPercentage(state);
-                            var bool = !(statePerc > start && statePerc < end);
-                            state.classed("disabled", bool);
-
-                        });
-
-
-                    d3.select("#slider")
-                        .attr("x", mousePosition);
-                } else if(!window.selected) {
-                    key.select("#slider")
-                        .attr("x", 0);
-
-                    d3.selectAll(".state").classed("disabled", false)
-
-                    d3.selectAll(".state")
-                        .classed("disabled", false)
-
-                    key.select("#slider")
-                        .attr("x", 0);
+                } else {
+                    _is_state_selected = true;
+                    d3.select("svg > g").transition()
+                        .duration(750)
+                        .style("stroke-width", 1.5 + "px")
+                        .style("stroke-color", "#f5f5f5")
+                        .attr("transform", state.selectionTransform);
                 }
             })
-
-        d3.select("path.domain")
-            .style("fill", "url(#primaryGradient)")
             .on("mouseover", function(){
-                window.over = true;
+                d3.select('span.tooltip')
+                    .text(state.name + " " + percentage)
+                    .style("display", "block")
+                    .style("left", 190 + state.textX + "px" )
+                    .style("top", -10 + state.topLeft[1] + "px" )
+                    .style("z-index", 1000);
+
+                d3.select(this).style("fill", state.dColor);
             })
+            .on("mouseout", function(){
+                d3.select('span.tooltip').style("display", "none");
 
-        var keyElements = d3.selectAll("g.key > *");
-        keyElements[0].reverse();
-        keyElements.order();
+                d3.select(this).style("fill", state.color);
+            })
+            .data(state);
 
-        key.append("rect")
-            .attr("height", 33)
-            .attr("width","2px")
-            .style("fill", "#000")
-            .attr("id", "slider");
-
-        d3.selectAll(".tick")
-            .append("rect")
-            .attr("height", 33)
-            .attr("width","1px")
-            .style("fill", "#fff")
-    }
-
-    var highlightState = function(){
-
-        var self, shape, id;
-
-        self = d3.select(this)
-        shape = (d3.select(this).node().nodeName == "LI") ?
-            d3.select("#" + d3.select(this).attr('data-state-id')) :
-            self;
-
-        shape.style('fill',
-            d3.rgb(shape.style('fill')).darker(.3)
-        );
-    }
-
-    var unhighlightState = function(){
-
-        var self, shape, id;
-
-        self = d3.select(this)
-        shape = (d3.select(this).node().nodeName == "LI") ?
-            d3.select("#" + d3.select(this).attr('data-state-id')) :
-            self;
-
-        shape.style('fill',
-            d3.rgb(shape.style('fill')).brighter(.3)
-        );
-    }
-
-    var statePercentage = function(shape){
-        return numeral(stateNumPercentage(shape)).format("0.0%");
-    }
-
-    var stateNumPercentage = function(shape){
-        var state = shape;
-        var value = parseInt(state.attr('data-val'), 10);
-        var total = parseInt(state.attr('data-total'), 10);
-        return value/total;
-    }
-
-    function click() {
-        window.selected = true;
-
-        if (d3.select(this).node().nodeName == "LI"){
-            var id = d3.select(this).attr('data-state-id');
-            d = d3.select("#" + id);
-        } else
-            d = d3.select(this);
-
-        if (active && d) return reset();
-
-        active = true;
-
-        g.selectAll(".active").classed("active", false);
-
-        g.selectAll(".state").classed("disabled", true);
-
-        d.classed("active", true).classed("disabled", false)
-
-        var n = d.node().getBBox();
-
-        topLeft = [n.x, n.y];
-        bottomRight = [n.x + n.width, n.y + n.height];
-
-        //Calculate the height and width of the box
-        stateHeight = Math.abs(n.height);
-        stateWidth = Math.abs(n.width);
-
-        centerX = n.x + (stateWidth / 2)
-        centerY = n.y + stateHeight / 2
-
-        /*drawDot(centerX, centerY);
-         drawDot(topLeft[0], topLeft[1]);
-         drawDot(bottomRight[0], bottomRight[1]);
-         drawDot(topLeft[0] + stateWidth, topLeft[1]);
-         drawDot(bottomRight[0] - stateWidth, bottomRight[1]);*/
-
-        var fontSize = "20px";
-        var translationDifferential = .40;
-        var xDifference = -23;
-        var yDifference = 10;
-
-        if (stateHeight < 10 || stateWidth < 10){
-            scale = .25 / Math.max( stateHeight / height, stateWidth / width);
-            fontSize = "6px";
-            translationDifferential = .30;
-            xDifference = -10;
-        } else if (stateHeight < 30 || stateWidth < 30) {
-            translationDifferential = .30;
-            scale = .50 / Math.max( stateHeight / height, stateWidth / width);
-            fontSize = "10px";
-        } else
-            scale = .50 / Math.max( stateHeight / height, stateWidth / width);
-
-        x = -topLeft[0]
-        y = -topLeft[1]
-
-        g.selectAll("path")
-            .classed("active", active && function(d) { return d === active; });
-
-        g.transition()
-            .duration(750)
-            .style("stroke-width", 1.5 / scale + "px")
-            .style("stroke-color", "#f5f5f5")
-            .attr("transform",
-                "skewX(15) translate(" + topLeft[0]*translationDifferential +","+ topLeft[1]*translationDifferential + ")"
-                    + "scale(" + scale + ")"
-                    + "translate(" + x + "," + y + ") ");
-
-        d3.select("svg").append("svg:text")
-            .attr("x", centerX + xDifference)
-            .attr("y", centerY + yDifference)
-            .text( statePercentage(d) )
-            .style("font-family", "sans-serif")
-            .style("font-size", 50)
-            .style("fill", "#000");
-
-
-    }
-
-    function reset() {
-        window.selected = false;
-
-        d3.selectAll("svg > text").data({}).exit().remove();
-
-        g.selectAll(".state")
-            .classed("disabled", false)
-            .classed("active", active = false);
-
-        g.transition().duration(750)
-            .attr("transform", "");
-    }
-
-    function drawDot(x, y) {
-        g.append("circle")
-            .attr("r", 2)
-            .attr("cx", x)
-            .attr("cy", y)
-            .attr("fill", "red")
-    }
-
-  var width = 959,
-    height = 650,
-    active;
-
-
-  var projection = d3.geo.albersUsa()
-    .scale(width)
-    .translate([0, 0]);
-
-  var path = d3.geo.path()
-    .projection(projection);
-
-  enrichDocument();
-
+        listView.append("li")
+            .attr("data-state-id", state.code)
+            .append("a")
+                .attr("href", "javascript:void(0)")
+                .text(state.name)
+                .append("span")
+                    .append("strong")
+                        .property("class", "perc")
+                        .text(percentage);
+    });
 })
